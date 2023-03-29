74b06183d837bfdd0e62dceb0a578d981dd5ea41
Galen Guyer <galen@galenguyer.com>
2023-03-28 22:22:54 -0400

    i am a web devloper (who needs to go home)

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,8 +1,15 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 
-use axum::{extract::Path, http::{StatusCode, HeaderMap}, response::IntoResponse, Extension};
-use tera::{Context, Tera};
+use axum::{
+    extract::Path,
+    http::{HeaderMap, StatusCode},
+    response::IntoResponse,
+    Extension,
+};
 use bytesize::ByteSize;
+use chrono::{DateTime, Utc};
+use serde::Serialize;
+use tera::{Context, Tera};
 
 pub async fn index(Extension(tera): Extension<Tera>) -> impl IntoResponse {
     let repos: Vec<String> = crate::db::sqlite::repositories::list()
@@ -12,16 +19,27 @@ pub async fn index(Extension(tera): Extension<Tera>) -> impl IntoResponse {
         .map(|r| r.name)
         .collect();
 
-    let categories = repos
-        .iter()
-        .filter(|r| r.contains("/"))
-        .map(|r| r.split("/").next().unwrap().to_string())
-        .collect::<HashSet<String>>();
-    let repos = repos
-        .iter()
-        .filter(|r| !r.contains("/"))
-        .map(|r| r.to_string())
-        .collect::<HashSet<String>>();
+    let categories = {
+        let mut c = repos
+            .iter()
+            .filter(|r| r.contains("/"))
+            .map(|r| r.split("/").next().unwrap().to_string())
+            .collect::<Vec<String>>();
+        c.dedup();
+        c.sort();
+        c
+    };
+
+    let repos = {
+        let mut r = repos
+            .iter()
+            .filter(|r| !r.contains("/"))
+            .map(|r| r.to_string())
+            .collect::<Vec<String>>();
+        r.dedup();
+        r.sort();
+        r
+    };
 
     let mut context = Context::new();
     context.insert("categories", &categories);
@@ -33,7 +51,18 @@ pub async fn index(Extension(tera): Extension<Tera>) -> impl IntoResponse {
     )
 }
 
-pub async fn repo(Path(name): Path<String>, Extension(tera): Extension<Tera>, headers: HeaderMap) -> impl IntoResponse {
+#[derive(Debug, Serialize)]
+struct TagGrouping {
+    tags: Vec<String>,
+    size: String,
+    manifest: String,
+    updated: DateTime<Utc>,
+}
+pub async fn repo(
+    Path(name): Path<String>,
+    Extension(tera): Extension<Tera>,
+    headers: HeaderMap,
+) -> impl IntoResponse {
     let repos: Vec<String> = crate::db::sqlite::repositories::list()
         .await
         .unwrap()
@@ -55,19 +84,59 @@ pub async fn repo(Path(name): Path<String>, Extension(tera): Extension<Tera>, he
         .collect::<HashSet<String>>();
 
     let tags = crate::db::sqlite::tags::list(&name).await.unwrap();
-    let mut sizes = HashMap::new();
-    for tag in tags.iter() {
-        let size = crate::db::sqlite::tags::get_size(&tag.manifest).await.unwrap_or_default();
-        let size = ByteSize::b(size as u64).to_string_as(true);
-        sizes.insert(tag.name.clone(), size);
+    let mut groupings: HashMap<String, TagGrouping> = HashMap::new();
+    for tag in tags {
+        match groupings.get_mut(&tag.manifest) {
+            Some(group) => {
+                group.tags.insert(0, tag.name.clone());
+                group.tags.sort_by(|a, b| {
+                    if a == "latest" {
+                        std::cmp::Ordering::Less
+                    } else if b == "latest" {
+                        std::cmp::Ordering::Greater
+                    } else {
+                        a.cmp(b)
+                    }
+                });
+                group.tags.dedup();
+            }
+            None => {
+                groupings.insert(
+                    tag.manifest.clone(),
+                    TagGrouping {
+                        tags: vec![tag.name.clone()],
+                        size: {
+                            let size = crate::db::sqlite::tags::get_size(&tag.manifest)
+                                .await
+                                .unwrap_or_default();
+                            ByteSize::b(size as u64).to_string_as(true)
+                        },
+                        manifest: tag.manifest.clone(),
+                        updated: tag.updated,
+                    },
+                );
+            }
+        }
     }
+    let mut groupings = groupings.values().collect::<Vec<&TagGrouping>>();
+    groupings.sort_by(|a, b| match a.updated.cmp(&b.updated) {
+        std::cmp::Ordering::Equal => {
+            if a.tags.contains(&"latest".to_string()) {
+                std::cmp::Ordering::Less
+            } else if b.tags.contains(&"latest".to_string()) {
+                std::cmp::Ordering::Greater
+            } else {
+                a.tags[0].cmp(&b.tags[0])
+            }
+        }
+        other => other,
+    });
 
     let mut context = Context::new();
     context.insert("name", &name.trim_matches('/'));
     context.insert("categories", &categories);
     context.insert("repos", &repos);
-    context.insert("tags", &tags);
-    context.insert("sizes", &sizes);
+    context.insert("groupings", &groupings);
     context.insert("host", headers.get("host").unwrap().to_str().unwrap());
 
     (
